0766d277f5e75f0459427075b2bf1d2c59379c16
Bartosz Kolasiński <kolasinski.bartosz@gmail.com>
2024-07-31 20:21:32 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/apcupsdevents/
 git pull
-cp /var/log/apcupsd.events /root/apcupsdevents/apcupsd.events
+cat /root/apcupsdevents/apcupsd.events /var/log/apcupsd.events | uniq > /root/apcupsdevents/apcupsd.events
 git add *
 git commit -m "auto"
 git push
