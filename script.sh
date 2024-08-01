5e6795bb001a905fdff29af3d72469bc501d5340
Bartosz Kolasiński <kolasinski.bartosz@gmail.com>
2024-08-01 16:25:33 +0200

    power_failure

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /root/apcupsdevents/
 git pull
-diff -u /root/apcupsdevents/apcupsd.events /var/log/apcupsd.events | grep -E '^\+[^+]'  | sed 's/^+//' >> /root/apcupsdevents/apcupsd.events
+diff -u /root/apcupsdevents/apcupsd.events /var/log/apcupsd.events | grep -E '^\+[^+]' | sed 's/^+//' >>/root/apcupsdevents/apcupsd.events
+cat apcupsd.events | grep -a "Power failure" >power_failure
 git add *
 git commit -m "auto"
 git push
